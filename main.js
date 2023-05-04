56d68d15fff5e9364b36abe37ea4da14bd647350
Allan Xavier <131976799+CoderAllan2013@users.noreply.github.com>
2023-05-04 19:28:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function span() {
+    console.log("This is an SPAN element");
+    document.getElementById("audio").pause();
+  }
+  
+  function div() {
+    console.log("This is an DIV element");
+    document.getElementById("audio").play();
+  }
